6a19b7504ed7b11a3d6a39eeee2337bfed5a3a96
Gerard Dróżdż <gerardd@interia.pl>
2022-01-03 20:43:18 +0100

    Reading JSON content in /lint requests

--- linter/main.go
+++ linter/main.go
@@ -1,28 +1,42 @@
 package main
 
 import (
-    "fmt"
     "log"
+    "io/ioutil"
+    "encoding/json"
     "net/http"
     "github.com/gorilla/mux"
 )
 
+type LintingRequest struct {
+    Content string `json:"content"`
+}
+
 func handleLint(responseWriter http.ResponseWriter, 
               request *http.Request){
-    fmt.Fprintf(responseWriter, "Hi, I'm linting!")
-    fmt.Println("Endpoint hit: /lint")
+    log.Println("Endpoint hit: /lint")
+
+    requestBody, _ := ioutil.ReadAll(request.Body)
+    var lintingRequest LintingRequest
+    json.Unmarshal(requestBody, &lintingRequest)
+
+    log.Println("Received content for linting:")
+    log.Println(lintingRequest.Content)
+
+    // TODO linting logic
+
+    json.NewEncoder(responseWriter).Encode(lintingRequest)
 }
 
 func handleHealthy(responseWriter http.ResponseWriter, 
               request *http.Request){
-    fmt.Fprintf(responseWriter, "Hi, I'm healthy!")
-    fmt.Println("Endpoint hit: /healthy")
+    log.Println("Endpoint hit: /healthy")
 }
 
 func serve() {
     myRouter := mux.NewRouter().StrictSlash(true)
-    myRouter.HandleFunc("/lint", handleLint)
-    myRouter.HandleFunc("/healthy", handleHealthy)
+    myRouter.HandleFunc("/lint", handleLint).Methods("POST")
+    myRouter.HandleFunc("/healthy", handleHealthy).Methods("GET")
     log.Fatal(http.ListenAndServe(":8136", myRouter))
 }
 
